83bb6a4f83c0055fa11a0abfa5e575618ca733f1
Sumit Parakh <sumit.thecoder@gmail.com>
2019-03-27 23:51:37 +0530

    feat(social): Added github profile link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Coming Soon</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <div>
+      <a href="https://github.com/sumitparakh" target="_blank" style={{color: "rebeccapurple"}} rel="noopener noreferrer">
+        <i class="fab fa-github fa-2x"></i>
+      </a>
+    </div><br/>
+    <h1>Coming Soon!!</h1>
   </Layout>
 )
 

3138af22c5d41fad9501d42fea3dafbed6dcca9f
Sumit Parakh <sumit.thecoder@gmail.com>
2019-07-29 00:13:27 +0530

    add to home name updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,11 +23,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Sumit Parakh`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#3f51b5`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },

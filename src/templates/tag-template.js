5c688cf6d624e384954047a528af23b25c78dcd7
Sumit Parakh <sumit.thecoder@gmail.com>
2019-07-31 23:22:13 +0530

    seo changes

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,24 +1,31 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+import SEO from "../components/SEO";
 
 function Tags(props) {
-  const posts = props.data.allMarkdownRemark.edges
-  const { tag } = props.pageContext
+    const posts = props.data.allMarkdownRemark.edges;
+    const { tag } = props.pageContext;
 
-  return (
-    <Layout>
-      <h1>{`Available posts in ${tag}`}</h1>
-      <div className="tags">
-        {posts.map(({ node }, i) => (
-          <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-        ))}
-      </div>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <SEO
+                title={`Available posts in ${tag}`}
+                description={`Available posts in ${tag}`}
+                url={props.location.href}
+                pathname={props.location.pathname}
+            />
+            <h1>{`Available posts in ${tag}`}</h1>
+            <div className="tags">
+                {posts.map(({ node }, i) => (
+                    <Link key={i} to={node.fields.slug}>{node.frontmatter.title}</Link>
+                ))}
+            </div>
+        </Layout>
+    )
 }
 
-export default Tags
+export default Tags;
 
 export const query = graphql`
   query TagsQuery($tag: String!) {
